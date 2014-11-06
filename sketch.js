bb15d9ab4ab26c808c238e509caaa5009a25e32e
gksktnsdl <gksktnsdl@naver.com>
2014-11-06 15:39:30 +0900

    dsfs

--- sketch.js
+++ sketch.js
@@ -51,3 +51,24 @@ function draw() {
 
  
 }
+
+
+
+
+function setup()
+
+     createCanvas(800,600);
+     noFill();
+     stroke(255,0,0);
+
+}
+
+function draw()
+{
+     background(0);
+     ellipse(400,300,mouseX,mouseY);
+}
+function setup() {
+    createCanvas(800, 800);
+}
+//이건 원래자리에에
